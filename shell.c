771dacee9a799937e4b47e9471ab5949e2922a39
Saad Khan <49029610+saad-khannn@users.noreply.github.com>
2019-03-28 12:46:13 -0400

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,4 +1,31 @@
-#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define BUILTINS 8
+#define T_BUFSIZE 256
+#define DELIM " \t\r\n\a"
+
+void shell();
+char *get_input();
+char **read_input(char *args);
+int exec(char **args);
+
+int cmd_cd(char **args);
+int cmd_clr();
+int cmd_dir(char **args);
+int cmd_echo(char **args);
+int cmd_environ();
+int cmd_help();
+int cmd_pause();
+int cmd_quit(char **args);
 
 char *builtin_cmds[] = {"cd", "clr", "dir", "environ", "echo", "help","pause", "quit"};
 int (*builtin_funcs[])(char **) = {&cmd_cd, &cmd_clr, &cmd_dir, &cmd_environ, &cmd_echo, &cmd_help, &cmd_pause, &cmd_quit};
